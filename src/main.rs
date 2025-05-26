ee8d5d868fbc8bc414d35ee44302d0d31c623d64
thijs <thijs@licht51.de>
2025-05-26 11:34:40 +0200

    hello world response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", axum::routing::get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .expect("Failed to bind TCP listener");
+
+    let server = axum::serve(listener, app);
+        
+
+    println!("Server running on http://localhost:3000");
+
+    server.await.expect("Failed to start server");
+
 }
